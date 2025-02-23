262e600aea7183840db4369af4fcbf01c1c04929
Marwan Mohamed <merolokamino@gmail.com>
2025-02-23 04:35:20 +0200

    installed gparted as a test

--- home/installs.nix
+++ home/installs.nix
@@ -9,6 +9,7 @@
         libnotify
         ffmpeg
         mpv
+        gparted
         # Managing Scripts for now 
         (writeShellScriptBin "pf" (builtins.readFile ../scripts/finder.sh))
         (writeShellScriptBin "bf" (builtins.readFile ../scripts/books.sh))
