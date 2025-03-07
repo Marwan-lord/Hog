2afdf06d82485bdb5df4d624e1f261c2842b837a
Marwan Mohamed <merolokamino@gmail.com>
2025-03-07 22:06:04 +0200

    added intelliJ for future java usage

--- home/installs.nix
+++ home/installs.nix
@@ -27,6 +27,7 @@
         lf
         rofi
         librewolf
+        jetbrains.idea-community
         # Managing Scripts for now 
         (writeShellScriptBin "pf" (builtins.readFile ../scripts/finder.sh))
         (writeShellScriptBin "bf" (builtins.readFile ../scripts/books.sh))
