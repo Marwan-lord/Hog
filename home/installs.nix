b30d8697343d529c75cd2e7af3151cab9f35120b
Marwan Mohamed <merolokamino@gmail.com>
2025-03-15 17:01:57 +0200

    full switch to helix

--- home/installs.nix
+++ home/installs.nix
@@ -29,6 +29,7 @@
         ghostty
         helix
         bacon
+        lldb
         # Managing Scripts for now 
         (writeShellScriptBin "pf" (builtins.readFile ../scripts/finder.sh))
         (writeShellScriptBin "bf" (builtins.readFile ../scripts/books.sh))
